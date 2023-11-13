f8393d4333d2016b6c58675eba715d70b07ff0f6
Sangamesh Kotni <sangam@gmail.com>
2023-11-13 22:12:24 +0000

    refactored code for hashicorp

---
+++ bucket.tf
@@ -0,0 +1,14 @@
+#resource "google_storage_bucket" "raw" {
+#  project                     = var.project
+#  name                        = "sangam-raw111111111"
+#  force_destroy               = false
+#  uniform_bucket_level_access = true
+#  location                    = var.region
+#  labels                      = local.labels
+#}
+#
+#locals {
+#  labels = {
+#    "data-project" = "sangam-raw111111111"
+#  }
+#}
