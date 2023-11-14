a99a6de89a3f85acba57b9d4875d713017119395
Sangamesh Kotni <sangam@gmail.com>
2023-11-14 21:38:13 +0000

    updated gke version

--- modules/google-gke-cluster/gke.tf
+++ modules/google-gke-cluster/gke.tf
@@ -24,25 +24,55 @@ data "google_container_engine_versions" "gke_version" {
   version_prefix = "1.27."
 }
 
-resource "google_container_node_pool" "vault_node_pool" {
+
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
   name       = "vault-node-pool"
-  location   = var.cluster_location
-  cluster    = google_container_cluster.primary.name
+  location = var.region
+  cluster  = google_container_cluster.primary.name
+
   version    = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
   node_count = var.initial_node_count
 
   node_config {
-    service_account = var.unseal_service_account
-    machine_type    = var.node_machine_type
-    image_type      = var.node_image_type
-
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloudkms",
-      "https://www.googleapis.com/auth/cloud-platform",
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    # preemptible  = true
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
 }
+#
+#
+#resource "google_container_node_pool" "vault_node_pool" {
+#  name       = "vault-node-pool"
+#  location   = var.cluster_location
+#  cluster    = google_container_cluster.primary.name
+#  version    = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+#  node_count = var.initial_node_count
+#
+#  node_config {
+#    service_account = var.unseal_service_account
+#    machine_type    = var.node_machine_type
+#    image_type      = var.node_image_type
+#
+#    oauth_scopes = [
+#      "https://www.googleapis.com/auth/cloudkms",
+#      "https://www.googleapis.com/auth/cloud-platform",
+#      "https://www.googleapis.com/auth/compute",
+#      "https://www.googleapis.com/auth/devstorage.read_only",
+#      "https://www.googleapis.com/auth/logging.write",
+#      "https://www.googleapis.com/auth/monitoring",
+#    ]
+#  }
+#}
